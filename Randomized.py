86c51db855cb8f3919f6fffd3cc39319459bad66
Soumya <39845943+soumya-mishra@users.noreply.github.com>
2018-06-07 22:34:18 +0530

    Randomized in python

---
+++ Randomized.py
@@ -0,0 +1,8 @@
+from random import shuffle
+x = ['Keep', 'The', 'Blue', 'Flag', 'Flying', 'High']
+shuffle(x)
+print(x)
+
+###The output of the following code is as below.
+
+['Flying', 'Keep', 'Blue', 'High', 'The', 'Flag']
